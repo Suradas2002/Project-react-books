12172a93d74e9b23baacde5746531eaa4e8145e9
Suradas2002 <suradas2002@gmail.com>
2024-05-19 15:56:28 +0700

    commit2

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/anime-project-gh/";
-  }
-
-  return config;
-});
+export default defineConfig({
+  server: {port:3000},
+  plugins: [react()],
+})
